ce9d04d40f84a143bcc8c3f525fdf7ae18942cda
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-24 15:29:35 -0400

    Classes created
    for both Active and Inactive game pieces.

---
+++ src/js/InactivePiece.js
@@ -0,0 +1,26 @@
+import Pieces from './Pieces';
+
+
+export default class InactivePiece extends Pieces {
+	constructor {
+		super ();
+		this.color =
+		this.Xposition =
+		this.Yposition =
+	};
+	Box2 = new Box () {
+		this.color = 
+		this.Xposition = 
+		this.Yposition =
+	};
+	Box3 = new Box () {
+		this.color = 
+		this.Xposition = 
+		this.Yposition =
+	};
+	Box4 = new Box () {
+		this.color = 
+		this.Xposition = 
+		this.Yposition =
+	};
+};
--- src/js/Pieces.js
+++ src/js/Pieces.js
@@ -1,13 +1,38 @@
-export default class Pieces {
+
+
+
+ export default class Pieces {
 
 	constructor () {
-		this.width: 
-		this.height:
-		this.XPosition:
-		this.YPosition:
+		this.width = 
+		this.height =
+	
 
 
 
 	}
 
 }
+//export default class InactiveBox extends Pieces {
+	//constructor {
+		//super ();
+		//this.color =
+		//this.Xposition =
+		//this.Yposition =
+	//};
+	//Box2 = new Box () {
+		//this.color = 
+		//this.Xposition = 
+		//this.Yposition =
+	//};
+	//Box3 = new Box () {
+		//this.color = 
+		//this.Xposition = 
+		//this.Yposition =
+	//};
+	//Box4 = new Box () {
+		//this.color = 
+		//this.Xposition = 
+		//this.Yposition =
+	//};
+//};
