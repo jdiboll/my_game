3841f3cc2e109fabc21da83cb52f6ac362cd12ae
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-27 15:31:21 -0400

    starting story and putting it on the screen

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
 // Javascript Entry Point
 import $ from 'jquery';
-import _ from 'lodash';
+import choices from ',/long_walk';
+
+class Game {
+	constructor(choices) {
+		this.choices = choices;
+		this.paths = []
+	};
+
+	slide(next) {
+		var buttons = next.paths.map(path = {
+			return `<li>button data-next="${path.next}">${path.label}</button></li>`;
+		})
+
+		return `
+		<h3>next.message</h3>
+		<ul>buttons.join('')<ul>
+		`
+	}
+	alltogether() {
+		return this.paths.reduce((a, b) = a + b);
+	}
+
+	upNext(choiceName) {
+		this.paths.push(choiceName.length);
+		var choice = this.choices[choiceName];
+		var choiceSlide = this.slide(next);
+		$('.gamePage').html(choiceSlide);
+	}
+}
