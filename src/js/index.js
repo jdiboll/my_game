23f1bea505ce9ba1464566d9c0afeeb1e6462297
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-27 14:12:44 -0400

    starting over. My reach is beyond my grasp.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
 // Javascript Entry Point
 import $ from 'jquery';
 import _ from 'lodash';
-import ActivePiece from './ActivePiece';
-import InactivePiece from './InactivePiece';
-import StartBox from './StartBox';
-import EndBox from './EndBox';
