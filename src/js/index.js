892b9b2d000f0e00fbfbb6d19fbd525e38ce255c
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-28 14:56:24 -0400

    additions...
    Progress bar added and targeted by button.
    Random damage applied to either side for varied outcome.

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,55 @@
 // Javascript Entry Point
 import $ from 'jquery';
+import _ from 'lodash';
+
+
+class Player {
+	constructor () {
+		this.health = 100;
+		this.damage = function(num) {
+			var hitPoints = num || 10;
+			return this.health = this.health - hitPoints;
+		}
+	}
+};
+	var goodGuy = new Player () 
+	var badGuy = new Player () 
+
+
+		
+
+	
+	var bgHealth = document.querySelector('#jesusHealth');
+
+$('button').on('click', function (){
+	var num1 = _.random(2,25);
+	var num2 = _.random(2,25);
+	// console.log(this);
+	//console.log(num);
+	// this.damage(num);
+	goodGuy.damage(num1);
+	console.log(goodGuy.health);
+	badGuy.damage(num2);
+	console.log(badGuy.health);
+
+	document.getElementById('walterHealth').innerHTML=(`
+		<progress value="${goodGuy.health}" max="100"></progress>
+	`);
+	
+
+	document.getElementById('jesusHealth').innerHTML=(`
+		<progress value="${badGuy.health}" max="100"></progress>
+	`);
 
 
 
+})
 
 
+// Left to MVP
+// 	progress bars to respond
+// 	game over
+// 	restart
 
 
 
