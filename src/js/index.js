853d782e7c2541e2e0c5b24151dbfaa74fd5edc9
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-27 20:21:53 -0400

    starting over

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,10 @@
 // Javascript Entry Point
 import $ from 'jquery';
-import choices from ',/long_walk';
 
-class Game {
-	constructor(choices) {
-		this.choices = choices;
-		this.paths = []
-	};
 
-	slide(next) {
-		var buttons = next.paths.map(path = {
-			return `<li>button data-next="${path.next}">${path.label}</button></li>`;
-		})
 
-		return `
-		<h3>next.message</h3>
-		<ul>buttons.join('')<ul>
-		`
-	}
-	alltogether() {
-		return this.paths.reduce((a, b) = a + b);
-	}
 
-	upNext(choiceName) {
-		this.paths.push(choiceName.length);
-		var choice = this.choices[choiceName];
-		var choiceSlide = this.slide(next);
-		$('.gamePage').html(choiceSlide);
-	}
-}
+
+
+
+
